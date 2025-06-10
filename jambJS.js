faf47839546dd7b36c5e65bcebca716bc906bceb
G3TECH1 <ebulug001@gmail.com>
2025-06-10 14:53:30 +0100

    Update jambJS.js

--- jambJS.js
+++ jambJS.js
@@ -2,8 +2,21 @@ var drop_down = document.getElementById("drop-down");
 var cancel = document.getElementById("CM");
 var hamburger_menu = document.querySelector("#hamburger-menu")
 const nest_list = document.querySelectorAll("li")
-//var sublink = document.querySelectorAll(".sublink")
+const external_link = document.querySelectorAll(
+    'a[href^="http"]:not([href*="' + window.location.hostname + '"])'
+);
 
+
+external_link.forEach(
+    link => {
+        link.addEventListener("click", function(event){
+            event.preventDefault()
+            if(confirm("This is a jamb replicated site, You are leaving our site. Do you want to continue?")){
+                window.location.href = this.href
+            };
+        });
+    }
+);
 hamburger_menu.addEventListener("click", function(){
     drop_down.style.display = "block";
     this.style.display = "none";
